f8babc1fd622b36025b6234731452a57ab296b2c
bednayb <bednayb@gmail.com>
2019-07-20 13:14:23 +0200

    add default the movie module (lazy loading)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,21 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "",
+    redirectTo: "/movies",
+    pathMatch: "full"
+  },
+  {
+    path: "movies",
+    loadChildren: "./movie/movie.module#MovieModule",
+  },
+  {
+    path: "**",
+    redirectTo: "movies",
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
