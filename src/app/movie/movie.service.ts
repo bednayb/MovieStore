6008aa05748d3a838a9424f8ea4b185ba1c0edf3
bednayb <bednayb@gmail.com>
2019-07-20 18:18:39 +0200

    load genre and transform genre ids to genre name

--- src/app/movie/movie.service.ts
+++ src/app/movie/movie.service.ts
@@ -1,3 +1,4 @@
+import { Genre } from './models/genre.model';
 import { MovieBasic } from './models/movie.basic.model';
 import { Injectable } from "@angular/core";
 import { BehaviorSubject } from "rxjs";
@@ -10,22 +11,37 @@ import { environment } from 'src/environments/environment';
 export class MovieService {
 
   // TODO: CHANGE PATH to DINAMICALLY
-  private path = "https://api.themoviedb.org/3/search/multi?api_key=" + environment.API_KEY + "&language=en-US&query=Kill%20Bill&page=1&include_adult=false";
+  private moviePath = "https://api.themoviedb.org/3/search/multi?api_key=" + environment.API_KEY + "&language=en-US&query=Kill%20Bill&page=1&include_adult=false";
+
   // MovieList
   private _movies = new BehaviorSubject<MovieBasic[]>([]);
-  
+
   get movies() {
     return this._movies.asObservable();
   }
 
+  // Genre
+  private genrePath = "https://api.themoviedb.org/3/genre/movie/list?api_key=" + environment.API_KEY + "&language=en-US"
+  private genreList: Genre[] = [];
+
   constructor(
     private http: HttpClient,
   ) {
+    this.getGenreList();
     this.search();
   }
 
+  getGenreList() {
+    return this.http.get<any>(this.genrePath).pipe(
+      tap(genreList => {
+        this.genreList = genreList.genres;
+      }
+      )
+    ).subscribe();
+  }
+
   search() {
-    return this.http.get<any>(this.path)
+    return this.http.get<any>(this.moviePath)
       .pipe(
         map(
           movies => movies.results
@@ -33,13 +49,23 @@ export class MovieService {
         map(
           movieList => {
             const moviesContainer = [];
-            movieList.map(y => {
+            movieList.map(movie => {
+              const temporaryGenreList = [];
+              movie.genre_ids.forEach(id => {
+                this.genreList.forEach((genre) => {
+                  if (genre.id === id) {
+                    temporaryGenreList.push(genre.name);
+                    return;
+                  }
+                });
+              });
+
               moviesContainer.push(
                 new MovieBasic(
-                  y.poster_path,
-                  y.title,
-                  y.release_date,
-                  y.genre_ids
+                  movie.poster_path,
+                  movie.title,
+                  movie.release_date,
+                  temporaryGenreList
                 )
               );
             });
