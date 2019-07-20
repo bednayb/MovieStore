7a32ed7949e79f64f877894c7eec0b3730425c0a
bednayb <bednayb@gmail.com>
2019-07-20 13:30:18 +0200

    add movie models

---
+++ src/app/movie/models/movie.basic.model.ts
@@ -0,0 +1,6 @@
+export interface MovieBasic {
+  poster_path: string;
+  title: string;
+  release_date: string;
+  genre: string[];
+}
---
+++ src/app/movie/models/movie.expanded.model.ts
@@ -0,0 +1,10 @@
+export interface MovieExpanded {
+  poster_path: string;
+  title: string;
+  description: string;
+  genre: string[];
+  release_date: string;
+  imdb_id: string;
+  runtime: number;
+  origin_country: string[];
+}
