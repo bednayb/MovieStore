126f45f330edde9dfe98c1978c344c8f97abe7ef
bednayb <bednayb@gmail.com>
2019-07-20 17:07:08 +0200

    add movieCard with basic style (genre not works yet)

--- src/app/movie/movie-card/movie-card.component.ts
+++ src/app/movie/movie-card/movie-card.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { MovieBasic } from './../models/movie.basic.model';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-movie-card',
@@ -6,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./movie-card.component.scss']
 })
 export class MovieCardComponent implements OnInit {
+
+  @Input()
+  public movieData: MovieBasic;
 
   constructor() { }
 
