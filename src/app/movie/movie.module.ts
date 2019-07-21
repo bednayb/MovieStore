9aa6956f4d26113c8560da1b1ba3bd2907822b50
bednayb <bednayb@gmail.com>
2019-07-21 08:36:18 +0200

    add reactive form

--- src/app/movie/movie.module.ts
+++ src/app/movie/movie.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MovieCardComponent } from './movie-card/movie-card.component';
 import { Routes, RouterModule } from "@angular/router";
+import { ReactiveFormsModule } from '@angular/forms';
 
 const routes: Routes = [
   {
@@ -12,11 +13,11 @@ const routes: Routes = [
   }
 ];
 
-
 @NgModule({
   declarations: [MovieCardComponent, MovieContainerComponent],
   imports: [
     CommonModule,
+    ReactiveFormsModule,
     RouterModule.forChild(routes)
   ],
   providers: [MovieService],
