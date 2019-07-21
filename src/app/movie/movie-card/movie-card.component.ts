23f955d23f01223924a33fc4be2077ba405a347f
bednayb <bednayb@gmail.com>
2019-07-21 07:26:53 +0200

    add click listener to open the details component

--- src/app/movie/movie-card/movie-card.component.ts
+++ src/app/movie/movie-card/movie-card.component.ts
@@ -1,5 +1,7 @@
+import { MatDialog } from '@angular/material';
 import { MovieBasic } from './../models/movie.basic.model';
 import { Component, OnInit, Input } from '@angular/core';
+import { MovieDetailsComponent } from '../movie-details/movie-details.component';
 
 @Component({
   selector: 'app-movie-card',
@@ -11,9 +13,19 @@ export class MovieCardComponent implements OnInit {
   @Input()
   public movieData: MovieBasic;
 
-  constructor() { }
+  constructor(
+    public dialog: MatDialog,
+  ) { }
 
   ngOnInit() {
+  
   }
 
+  openDetails(): void {
+    this.dialog.open(MovieDetailsComponent, {
+      data: { id: this.movieData.id }
+    });
+
+
+  }
 }
