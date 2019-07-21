843975c2073ff140af49b0287642dbb2850d61bd
bednayb <bednayb@gmail.com>
2019-07-21 07:27:23 +0200

    add details component

--- src/app/movie/movie-details/movie-details.component.ts
+++ src/app/movie/movie-details/movie-details.component.ts
@@ -1,15 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { MovieService } from './../movie.service';
+import { Component, OnInit, OnDestroy, Inject } from '@angular/core';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
+import { MovieExpanded } from '../models/movie.expanded.model';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-movie-details',
   templateUrl: './movie-details.component.html',
   styleUrls: ['./movie-details.component.scss']
 })
-export class MovieDetailsComponent implements OnInit {
+export class MovieDetailsComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  // Movie
+  private movieSub: Subscription;
+  public movieData: MovieExpanded;
+
+  constructor(
+    public dialogRef: MatDialogRef<MovieDetailsComponent>,
+    private movieService: MovieService,
+    @Inject(MAT_DIALOG_DATA) public data: any
+  ) { }
 
   ngOnInit() {
+    this.movieService.searchMovie(this.data.id);
+    this.movieSub = this.movieService.movie.subscribe(movieData => {
+      this.movieData = movieData;
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.movieSub.unsubscribe();
   }
 
 }
