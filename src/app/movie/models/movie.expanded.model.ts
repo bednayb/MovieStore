a4cf03b4728984fb97bb453ed82787791da8d3ce
bednayb <bednayb@gmail.com>
2019-07-21 07:22:52 +0200

    fix: change structure

--- src/app/movie/models/movie.expanded.model.ts
+++ src/app/movie/models/movie.expanded.model.ts
@@ -1,10 +1,15 @@
-export interface MovieExpanded {
-  poster_path: string;
-  title: string;
-  description: string;
-  genre: string[];
-  release_date: string;
-  imdb_id: string;
-  runtime: number;
-  origin_country: string[];
+export class MovieExpanded {
+  constructor(
+    public poster_path: string,
+    public title: string,
+    public language: string[],
+    public tagline: string,
+    public description: string,
+    public genres: string[],
+    public release_date: string,
+    public imdb_id: string,
+    public runtime: number,
+    public production_countries: string[]
+  ) { }
+
 }
